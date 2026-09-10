43fcf2bf865ef8786e4c959da9368e48ebe86851
lunxkoe <lunxkoe@gmail.com>
2026-09-10 18:40:57 +0900

    finish: C02/S07

---
+++ C02_Intermediate_02/S07/src/set/HashStart5.java
@@ -0,0 +1,54 @@
+package set;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+
+public class HashStart5 {
+
+    static final int CAPACITY = 10;
+
+    public static void main(String[] args) {
+        LinkedList<Integer>[] buckets = new LinkedList[CAPACITY];
+        System.out.println("buckets = " + Arrays.toString(buckets));
+        for (int i = 0; i < CAPACITY; i++) {
+            buckets[i] = new LinkedList<>();
+        }
+
+        System.out.println("buckets = " + Arrays.toString(buckets));
+
+        add(buckets, 1);
+        add(buckets, 2);
+        add(buckets, 5);
+        add(buckets, 8);
+        add(buckets, 14);
+        add(buckets, 99);
+        add(buckets, 9); // 중복
+
+        System.out.println("buckets = " + Arrays.toString(buckets));
+
+        // 검색
+        int searchValue = 9;
+        boolean contains = contains(buckets, searchValue);
+        System.out.println("contains = " + contains);
+    }
+
+    private static void add(LinkedList<Integer>[] buckets, int value) {
+        int hashIndex = hashIndex(value);
+        LinkedList<Integer> bucket = buckets[hashIndex]; // O(1)
+
+        // 중복 확인
+        if (!bucket.contains(value)) {
+            bucket.add(value);
+        }
+    }
+
+    private static boolean contains(LinkedList<Integer>[] buckets, int searchValue) {
+        int hashIndex = hashIndex(searchValue);
+        LinkedList<Integer> bucket = buckets[hashIndex]; // O(1)
+        return bucket.contains(searchValue); // O(N)
+    }
+
+    static int hashIndex(int value) {
+        return value % CAPACITY;
+    }
+}
---
+++ C02_Intermediate_02/S07/src/set/MyHashSetV0.java
@@ -0,0 +1,44 @@
+package set;
+
+import java.util.Arrays;
+
+public class MyHashSetV0 {
+
+    private int[] elementData = new int[10];
+    private int size = 0;
+
+    // O(N)
+    public boolean add(int value) {
+        // 중복 확인
+        if (contains(value)) {
+            return false;
+        }
+
+        // 데이터 추가
+        elementData[size] = value;
+        size++;
+        return true;
+    }
+
+    // O(N)
+    public boolean contains(int value) {
+        for (int data : elementData) {
+            if (data == value) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public String toString() {
+        return "MyHashSetV0{" +
+                "elementData=" + Arrays.toString(Arrays.copyOf(elementData, size)) +
+                ", size=" + size +
+                '}';
+    }
+}
