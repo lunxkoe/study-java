20660e70762dca24d9ecbd860fd60f934888fb9b
lunxkoe <lunxkoe@gmail.com>
2026-09-23 18:51:48 +0900

    finish: C04/S02

---
+++ C04_Advanced_02/java-adv2/src/charset/EncodingMain2.java
@@ -0,0 +1,61 @@
+package charset;
+
+import javax.print.attribute.standard.MediaSize;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+
+import static java.nio.charset.StandardCharsets.*;
+
+public class EncodingMain2 {
+
+    private static final Charset EUC_KR = Charset.forName("EUC_KR");
+    private static final Charset MS_949 = Charset.forName("MS_949");
+
+    public static void main(String[] args) {
+        System.out.println("== 영문 ASCII 인코딩 ==");
+        test("A", US_ASCII, US_ASCII);
+        test("A", US_ASCII, ISO_8859_1);
+        test("A", US_ASCII, EUC_KR);
+        test("A", US_ASCII, MS_949);
+        test("A", US_ASCII, UTF_8);
+        test("A", US_ASCII, UTF_16BE);
+
+        System.out.println("== 한글 인코딩 - 기본 ==");
+        test("가", US_ASCII, US_ASCII);
+        test("가", ISO_8859_1, ISO_8859_1);
+        test("가", EUC_KR, EUC_KR);
+        test("가", MS_949, MS_949);
+        test("가", UTF_8, UTF_8);
+        test("가", UTF_16BE, UTF_16BE);
+
+        System.out.println("== 한글 인코딩 - 복잡한 문자 ==");
+        test("뷁", EUC_KR, EUC_KR);
+        test("뷁", MS_949, MS_949);
+        test("뷁", UTF_8, UTF_8);
+        test("뷁", UTF_16BE, UTF_16BE);
+
+        System.out.println("== 한글 인코딩 - 디코딩이 다른 경우 ==");
+        test("가", EUC_KR, MS_949); // MS_949가 EUC_KR을 받아서 되었기 때문에 디코딩 가능
+        test("뷁", MS_949, EUC_KR); // 인코딩은 가능한데, 디코딩은 안됨 - 특정 문자는 됨
+        test("가", EUC_KR, UTF_8);
+        test("가", MS_949, UTF_8);
+        test("가", UTF_8, UTF_16BE);
+        test("가", UTF_16BE, UTF_8);
+
+        System.out.println("== 영문 인코딩 - 디코딩이 다른 경우 ==");
+        test("A", EUC_KR, UTF_8);
+        test("A", MS_949, UTF_8);
+        test("A", UTF_8, UTF_16BE);
+    }
+
+    private static void test(String text, Charset encodingCharset, Charset decodingCharset) {
+        byte[] encoded = text.getBytes(encodingCharset);
+        String decoded = new String(encoded, decodingCharset);
+        System.out.printf("%s -> [%s] 인코딩 -> %s %sbyte -> [%s] 디코딩 -> %s\n",
+                text,
+                encodingCharset, Arrays.toString(encoded), encoded.length,
+                decodingCharset, decoded
+        );
+    }
+}
